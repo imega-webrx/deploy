73bd5d0242ecc6863b452530c7e5f918861ca239
Dmitry Stoletov <irvis@imega.ru>
2021-02-23 02:14:50 +0300

    up conf
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

---
+++ root/deploy-backend.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -o allexport; source .env; set +o allexport
+
+curl -s https://codeload.github.com/imega-webrx/backend/zip/master -o /root/backend.zip || exit 1
+
+unzip -x /root/backend.zip
+
+rm /root/backend.zip
+
+cd /root/backend-master
+
+npm ci
+
+rm -rf /srv/backend/*
+
+mv /root/backend-master/* /srv/backend/
+
+pm2 del /srv/backend/ecosystem.config.js
+
+pm2 start /srv/backend/ecosystem.config.js
+
+rm -rf /root/backend-master
