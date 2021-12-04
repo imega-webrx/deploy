cc28e06de8a9b39483509b688f06a6092eb8f963
Dmitry Stoletov <irvis@imega.ru>
2021-12-04 18:16:43 +0300

    use http_realip_module
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

---
+++ conf/etc/cron.hourly/check_url.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -o allexport; source /root/.env; set +o allexport
+
+TEXT="<b>Check services:</b>"
+NL="%0A"
+
+assert() {
+    TEXT="${TEXT}${NL}$3"
+
+    if [ "$1" != "$2" ]; then
+        TEXT="${TEXT}<b>Fail</b>"
+    else
+        TEXT="${TEXT}Ok"
+    fi
+}
+
+ACTUAL=$(curl --write-out %{http_code} --silent --output /dev/null https://webrx.ru)
+
+assert 200 $ACTUAL "webrx.ru - "
+
+curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendMessage \
+    -H "Content-Type: application/x-www-form-urlencoded; charset=utf-8" \
+    -d chat_id=$CHAT_GROUP -d parse_mode=HTML -d text="${TEXT}"
