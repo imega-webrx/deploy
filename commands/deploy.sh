5b36a355b9234d756c75b6eec652067b9c556a1e
Dmitry Stoletov <irvis@imega.ru>
2021-12-04 15:24:22 +0300

    up deploy command
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -7,11 +7,11 @@ curl -s https://codeload.github.com/imega-webrx/deploy/zip/master \
     -o /root/tmp/deploy.zip || exit 1
 
 unzip -x /root/tmp/deploy.zip
-rm /root/deploy/deploy.zip
+rm -v /root/tmp/deploy.zip
+
+echo 22223
+
 rm -rv /root/deploy
-
-echo 2222
-
 mv -v /root/tmp/deploy-master /root/deploy
 
 echo 11111
