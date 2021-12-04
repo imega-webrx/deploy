b25c18202154418f23a95bc1e31feeae91a601e7
Dmitry Stoletov <irvis@imega.ru>
2021-12-04 15:15:09 +0300

    up deploy command
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

---
+++ commands/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o allexport; source .env; set +o allexport
+
+mkdir -pv /root/deploy /root/tmp
+cd /root/tmp
+curl -s https://codeload.github.com/imega-webrx/deploy/zip/master \
+    -o /root/tmp/deploy.zip || exit 1
+
+unzip -x /root/tmp/deploy.zip
+rm /root/deploy/deploy.zip
+rm -rv /root/deploy
+mv /root/tmp/deploy-master /root/deploy
+
+# systemctl restart webhook
+
+rm -rv /root/tmp
+
+curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendMessage \
+    -d chat_id=$CHAT_GROUP -d text="deploy deployment is completed."
