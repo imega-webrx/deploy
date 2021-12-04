3db9448ae1796f03b065e0f7cf5b724a2f07c1a0
Dmitry Stoletov <irvis@imega.ru>
2021-12-04 16:21:41 +0300

    up deploy command
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -11,27 +11,27 @@ curl -s https://codeload.github.com/imega-webrx/deploy/zip/master \
 
 unzip -x /root/tmp/deploy.zip
 rm -v /root/tmp/deploy.zip
+rm -rv /root/deploy
+mv -v /root/tmp/deploy-master /root/deploy
+chmod +x -R /root/deploy/commands
+rm -rv /root/tmp
 
 diff -q /root/deploy/lib/systemd/system/webhook.service \
     /lib/systemd/system/webhook.service || \
     {
         echo "Update /lib/systemd/system/webhook.service";
         TEXT="${TEXT}${NL}Update: /lib/systemd/system/webhook.service - "
+        cp -v /root/deploy/lib/systemd/system/webhook.service \
+            /lib/systemd/system/webhook.service && \
+            TEXT="${TEXT}Ok" || TEXT="${TEXT}<b>Fail</b>"
 
         echo "Reload systemd"
         TEXT="${TEXT}${NL}Reload systemd: "
-        systemctl daemon-reload && TEXT="${TEXT}Ok" || TEXT="${TEXT}Fail"
+        systemctl daemon-reload && TEXT="${TEXT}Ok" || TEXT="${TEXT}<b>Fail</b>"
     }
-
-rm -rv /root/deploy
-mv -v /root/tmp/deploy-master /root/deploy
-
-chmod +x -R /root/deploy/commands
 
 # systemctl restart webhook
 
-rm -rv /root/tmp
-
 curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendMessage \
     -H "Content-Type: application/x-www-form-urlencoded; charset=utf-8" \
-    -d chat_id=$CHAT_GROUP -d parse_mode=HTML -d text=$TEXT
+    -d chat_id=$CHAT_GROUP -d parse_mode=HTML -d text="${TEXT}"
