80fc3112c2b3ea43fe3c9a9e057f1ddeeda8665a
Dmitry Stoletov <irvis@imega.ru>
2021-12-04 16:58:24 +0300

    up deploy command
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 set -o allexport; source .env; set +o allexport
 
 TEXT="<b>Deploy deployment is completed</b>"
@@ -16,22 +16,81 @@ mv -v /root/tmp/deploy-master /root/deploy
 chmod +x -R /root/deploy/commands
 rm -rv /root/tmp
 
-diff -q /root/deploy/lib/systemd/system/webhook.service \
+RELOAD_SYSTEMCTL=false
+diff -q /root/deploy/conf/lib/systemd/system/webhook.service \
     /lib/systemd/system/webhook.service || \
     {
         echo "Update /lib/systemd/system/webhook.service";
         TEXT="${TEXT}${NL}Update: /lib/systemd/system/webhook.service - "
-        cp -v /root/deploy/lib/systemd/system/webhook.service \
+        cp -v /root/deploy/conf/lib/systemd/system/webhook.service \
             /lib/systemd/system/webhook.service && \
             TEXT="${TEXT}Ok" || TEXT="${TEXT}<b>Fail</b>"
-
-        echo "Reload systemd"
-        TEXT="${TEXT}${NL}Reload systemd: "
-        systemctl daemon-reload && TEXT="${TEXT}Ok" || TEXT="${TEXT}<b>Fail</b>"
+        RELOAD_SYSTEMCTL=true
     }
 
-# systemctl restart webhook
+diff -q /root/deploy/conf/etc/systemd/webhook \
+    /etc/systemd/webhook || \
+    {
+        echo "Update /root/deploy/conf/etc/systemd/webhook";
+        TEXT="${TEXT}${NL}Update: /etc/systemd/webhook - "
+        cp -v /root/deploy/conf/etc/systemd/webhook \
+            /etc/systemd/webhook && \
+            TEXT="${TEXT}Ok" || TEXT="${TEXT}<b>Fail</b>"
+        RELOAD_SYSTEMCTL=true
+    }
+
+RELOAD_SYSTEMCTL && {
+    echo "Reload systemd";
+    TEXT="${TEXT}${NL}Reload systemd: ";
+    systemctl daemon-reload && TEXT="${TEXT}Ok" || TEXT="${TEXT}<b>Fail</b>"
+}
+
+RELOAD_NGINX=false
+
+diff -q /root/deploy/conf/etc/nginx/nginx.conf \
+    /etc/nginx/nginx.conf || \
+    {
+        echo "Update /etc/nginx/nginx.conf";
+        TEXT="${TEXT}${NL}Update: /etc/nginx/nginx.conf - "
+        cp -v /root/deploy/conf/etc/nginx/nginx.conf \
+            /etc/nginx/nginx.conf && \
+            TEXT="${TEXT}Ok" || TEXT="${TEXT}<b>Fail</b>"
+        RELOAD_NGINX=true
+    }
+
+diff -q /root/deploy/conf/etc/nginx/conf.d/webrx.ru.conf \
+    /etc/nginx/conf.d/webrx.ru.conf || \
+    {
+        echo "Update /etc/nginx/conf.d/webrx.ru.conf";
+        TEXT="${TEXT}${NL}Update: /etc/nginx/conf.d/webrx.ru.conf - "
+        cp -v /root/deploy/conf/etc/nginx/conf.d/webrx.ru.conf \
+            /etc/nginx/conf.d/webrx.ru.conf && \
+            TEXT="${TEXT}Ok" || TEXT="${TEXT}<b>Fail</b>"
+        RELOAD_NGINX=true
+    }
+
+RELOAD_NGINX && {
+    echo "Reload nginx";
+    TEXT="${TEXT}${NL}Reload nginx: ";
+    systemctl reload nginx && TEXT="${TEXT}Ok" || TEXT="${TEXT}<b>Fail</b>"
+}
+
+RESTART_WEBHOOK=false
+diff -q /root/deploy/conf/etc/webhook/hooks.json \
+    /etc/webhook/hooks.json || \
+    {
+        echo "Update /etc/webhook/hooks.json";
+        TEXT="${TEXT}${NL}Update /etc/webhook/hooks.json - "
+        cp -v /root/deploy/conf/etc/webhook/hooks.json \
+            /etc/webhook/hooks.json && \
+            TEXT="${TEXT}Ok" || TEXT="${TEXT}<b>Fail</b>"
+        echo "Reload webhook"
+        TEXT="${TEXT}${NL}Reload webhook: Ok"
+        RESTART_WEBHOOK=true
+    }
 
 curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendMessage \
     -H "Content-Type: application/x-www-form-urlencoded; charset=utf-8" \
     -d chat_id=$CHAT_GROUP -d parse_mode=HTML -d text="${TEXT}"
+
+RESTART_WEBHOOK && systemctl restart webhook
